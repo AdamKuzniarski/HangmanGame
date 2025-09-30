8c97eda38ed8c2c81aafea93b758db77afd06490
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-09-30 10:38:32 +0200

    add fonts

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const WORDS = [
+  "suggestion",
+  "device",
+  "database",
+  "speaker",
+  "salad",
+  "ear",
+  "strategy",
+  "product",
+  "politics",
+  "category",
+  "housing",
+  "committee",
+  "garbage",
+  "assistance",
+  "discussion",
+];
+
+
